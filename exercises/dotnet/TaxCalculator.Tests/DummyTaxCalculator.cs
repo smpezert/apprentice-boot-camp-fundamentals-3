8fc5aab5e76ac11c10e7a1ea86a0b87dfb5d528b
Sofia Bezertzi <sofia_bezertzi@waters.com>
2023-01-31 15:46:35 +0000

    refactor dummy tax calulator to avoid conditions and renew new tax in diesel tests

--- exercises/dotnet/TaxCalculator.Tests/DummyTaxCalculator.cs
+++ exercises/dotnet/TaxCalculator.Tests/DummyTaxCalculator.cs
@@ -41,7 +41,7 @@ namespace TaxCalculator.Tests
 
             else if (vehicle.FuelType.Equals(FuelType.Diesel))
             {
-                cost = CalculateDieselTax(vehicle);
+                cost = GetTaxBandFromEmissions(emissions, DieselPriceIndex.index);
             }
 
             else if(fuelType.Equals(FuelType.AlternativeFuel))
@@ -91,35 +91,6 @@ namespace TaxCalculator.Tests
                 return 1240;
             else if (vehicle.Co2Emissions == 255)
                 return 1760;
-            else if (vehicle.Co2Emissions > 255)
-                return 2070;
-            else return 0;
-        }
-
-        private static int CalculateDieselTax(Vehicle vehicle)
-        {
-            if (vehicle.Co2Emissions > 49 && vehicle.Co2Emissions < 75)
-                return 25;
-            else if (vehicle.Co2Emissions > 74 && vehicle.Co2Emissions < 90)
-                return 105;
-            else if (vehicle.Co2Emissions > 89 && vehicle.Co2Emissions < 100)
-                return 125;
-            else if (vehicle.Co2Emissions > 99 && vehicle.Co2Emissions < 110)
-                return 145;
-            else if (vehicle.Co2Emissions > 109 && vehicle.Co2Emissions < 130)
-                return 165;
-            else if (vehicle.Co2Emissions > 129 && vehicle.Co2Emissions < 150)
-                return 205;
-            else if (vehicle.Co2Emissions > 149 && vehicle.Co2Emissions < 170)
-                return 515;
-            else if (vehicle.Co2Emissions > 169 && vehicle.Co2Emissions < 190)
-                return 830;
-            else if (vehicle.Co2Emissions > 189 && vehicle.Co2Emissions < 225)
-                return 1240;
-            else if (vehicle.Co2Emissions == 225)
-                return 1760;
-            else if (vehicle.Co2Emissions == 255)
-                return 2070;
             else if (vehicle.Co2Emissions > 255)
                 return 2070;
             else return 0;
